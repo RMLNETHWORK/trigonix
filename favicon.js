96a23ed7f05788d9cc79ea20b38a5451fade05ea
RMLNETHWORK <rmlnethwork@gmail.com>
2025-05-17 08:56:31 +0800

    ADDED ICON

---
+++ favicon.js
@@ -0,0 +1,8 @@
+// favicon.js
+(function() {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/x-icon';
+  link.href = 'icon.ico';
+  document.head.appendChild(link);
+})();
